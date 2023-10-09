f05ee12ccb45c2bfdf863e21a861b571f39b06c0
Michael Leisch <michael.leisch@gmx.at>
2023-10-09 21:41:42 +0200

    paar kommentare jaja

--- process.js
+++ process.js
@@ -1,12 +1,15 @@
 let currentBlock = new Array(2); // [x][y]
 let preDropBlock =  Array.from(Array(4), () => new Array(2));
-let rotateVariations;   // Current Variations of Rotation
-let rotateIndex = 1;    // Current Rotation Status
+let rotateVariations; // Current Variations of Rotation
+let rotateIndex = 1; // Current Rotation Status
 
 currentBlock = randomBlock();
 let intervalF = setInterval(fallGuys, 1000);
 preDrop();
 
+/**
+ * free blocks are falling down
+ */
 function fallGuys() {
     if (currentBlock[0][0][1] < 19 && currentBlock[0][1][1] < 19 && currentBlock[0][2][1] < 19 && currentBlock[0][3][1] < 19) { // Checks if next block isn't the ground
         if (checkCollision(0, 1)) { // Checks next block
@@ -15,7 +18,7 @@ function fallGuys() {
                 currentBlock[0][i][1]++; // y-axis increases by 1
             }
             for (let i = 0; i < currentBlock[0].length; i++) {
-                gameBoard[currentBlock[0][i][1]][currentBlock[0][i][0]] = currentBlock[1]; // Set updatet block
+                gameBoard[currentBlock[0][i][1]][currentBlock[0][i][0]] = currentBlock[1]; // Set updated block
             }
         }
     }
@@ -24,8 +27,11 @@ function fallGuys() {
     }
 }
 
+/**
+ * rotates the current block into the next position
+ */
 function rotateBlock() {
-    let midX, midY; // x- and y-achis of the focus block
+    let midX, midY; // x- and y-axis of the focus block
     rotateVariations = rotation(currentBlock[1])
 
     if (rotateIndex > 3) {
@@ -33,35 +39,85 @@ function rotateBlock() {
     }
     let rotations = rotateVariations[rotateIndex++];
 
-    if (checkRotation(rotations)) {
-        for (let i = 0; i < gameBoard.length; i++) {
+
+    if (checkRotation(rotations)) { // if rotation is possible
+        for (let i = 0; i < gameBoard.length; i++) { // clears all moving blocks
             for (let j = 0; j < gameBoard[0].length; j++) {
-                if (gameBoard[i][j].charAt(1) == "M") {
-                    gameBoard[i][j] = ""; // Clear all moving blocks
+                if (gameBoard[i][j].charAt(1) === 'M') {
+                    gameBoard[i][j] = "";
                 }
             }
         }
 
-        for (let i = 0; i < currentBlock[0].length; i++) {
-            if (currentBlock[0][i][2] == "M") {
-                midX = currentBlock[0][i][0];
-                midY = currentBlock[0][i][1];
-                gameBoard[midY][midX] = currentBlock[1] // Set focus block
+        for (let i = 0; i < currentBlock[0].length; i++) { // set focus block (middle block)
+            if (currentBlock[0][i][2] === 'M') {
+                midX = currentBlock[0][i][0]; // x-axis of the focus block
+                midY = currentBlock[0][i][1]; // y-axis of the focus block
+                gameBoard[midY][midX] = currentBlock[1]
             }
         }
 
         let count = 0;
-        for (let i = 0; i < currentBlock[0].length; i++) {
-            if (currentBlock[0][i][2] != "M") {
+        for (let i = 0; i < currentBlock[0].length; i++) { // set surrounding blocks
+            if (currentBlock[0][i][2] !== 'M') {
                 currentBlock[0][i][0] = midX + rotations[count][0];
                 currentBlock[0][i][1] = midY + rotations[count][1];
-                gameBoard[midY + rotations[count][1]][midX + rotations[count][0]] = currentBlock[1]; // Set other blocks
+                gameBoard[midY + rotations[count][1]][midX + rotations[count][0]] = currentBlock[1];
                 count++;
             }
         }
     }
     loadGameBoard(true);
 }
+
+/**
+ * checks if the rotation is possible
+ * @param rotations
+ * @returns {boolean}
+ */
+function checkRotation(rotations) {
+    let tempBlock = Array.from(Array(4), () => new Array(2)); // [x][y]
+    let midX, midY;
+
+    for (let i = 0; i < currentBlock[0].length; i++) {
+        if (currentBlock[0][i][2] === 'M') {
+            midX = currentBlock[0][i][0];
+            midY = currentBlock[0][i][1];
+        }
+    }
+
+    let count = 0;
+    for (let i = 0; i < currentBlock[0].length; i++) {
+        if (currentBlock[0][i][2] !== 'M') {
+            tempBlock[i][0] = midX + rotations[count][0];
+            tempBlock[i][1] = midY + rotations[count][1];
+            count++;
+        } else {
+            tempBlock[i][0] = midX;
+            tempBlock[i][1] = midY;
+        }
+    }
+
+    for (let i = 0; i < tempBlock.length; i++) {
+        if (tempBlock[i][1] > 19) {
+            fixBlock();
+            return false;
+        }
+        if (tempBlock[i][0] < 0 || tempBlock[i][0] > 19) {
+            return false;
+        }
+        if (gameBoard[tempBlock[i][1]][tempBlock[i][0]] !== "" && gameBoard[tempBlock[i][1]][tempBlock[i][0]] !== currentBlock[1]) {
+            return false;
+        }
+    }
+    return true;
+}
+
+/**
+ * function to get all rotation phases of any block you want
+ * @param thisBlock block ype
+ * @returns {any[][]} all rotation phases
+ */
 function rotation(thisBlock) {
     let rotate = Array.from(Array(4), () => new Array(4)); // [x][y]
     switch (thisBlock.charAt(0)) {
@@ -86,41 +142,6 @@ function rotation(thisBlock) {
     }
     return rotate
 }
-function checkRotation(rotations) {
-    let tempBlock = Array.from(Array(4), () => new Array(2)); // [x][y]
-    for (let i = 0; i < currentBlock[0].length; i++) {
-        if (currentBlock[0][i][2] == "M") {
-            midX = currentBlock[0][i][0];
-            midY = currentBlock[0][i][1];
-        }
-    }
-
-    let count = 0;
-    for (let i = 0; i < currentBlock[0].length; i++) {
-        if (currentBlock[0][i][2] != "M") {
-            tempBlock[i][0] = midX + rotations[count][0];
-            tempBlock[i][1] = midY + rotations[count][1];
-            count++;
-        } else {
-            tempBlock[i][0] = midX;
-            tempBlock[i][1] = midY;
-        }
-    }
-
-    for (let i = 0; i < tempBlock.length; i++) {
-        if (tempBlock[i][1] > 19) {
-            fixBlock();
-            return false;
-        }
-        if (tempBlock[i][0] < 0 || tempBlock[i][0] > 19) {
-            return false;
-        }
-        if (gameBoard[tempBlock[i][1]][tempBlock[i][0]] != "" && gameBoard[tempBlock[i][1]][tempBlock[i][0]] != currentBlock[1]) {
-            return false;
-        }
-    }
-    return true;
-}
 
 function preDrop() {
     for(let i = 0; i < currentBlock[0].length; i++){
@@ -130,19 +151,19 @@ function preDrop() {
 
     for (let i = 0; i < gameBoard.length; i++) {
         for(let j = 0; j < gameBoard[0].length; j++){
-            if(gameBoard[i][j].charAt(1) == "R"){
+            if(gameBoard[i][j].charAt(1) === "R"){
                 gameBoard[i][j] = "";
             }
         }
     }
 
     let fix = false;
-    do{
+    do{ // hilfe is des hässlich
         if(preDropBlock[0][1] < 19 && preDropBlock[1][1] < 19 && preDropBlock[2][1] < 19 && preDropBlock[3][1] < 19){
-            if (gameBoard[preDropBlock[0][1] + 1][preDropBlock[0][0]] == "" || gameBoard[preDropBlock[0][1] + 1][preDropBlock[0][0]] == currentBlock[1]) {
-                if(gameBoard[preDropBlock[1][1] + 1][preDropBlock[1][0]] == "" || gameBoard[preDropBlock[1][1] + 1][preDropBlock[1][0]] == currentBlock[1]){
-                    if(gameBoard[preDropBlock[2][1] + 1][preDropBlock[2][0]] == "" || gameBoard[preDropBlock[2][1] + 1][preDropBlock[2][0]] == currentBlock[1]){
-                        if(gameBoard[preDropBlock[3][1] + 1][preDropBlock[3][0]] == "" || gameBoard[preDropBlock[3][1] + 1][preDropBlock[3][0]] == currentBlock[1]){
+            if (gameBoard[preDropBlock[0][1] + 1][preDropBlock[0][0]] === "" || gameBoard[preDropBlock[0][1] + 1][preDropBlock[0][0]] === currentBlock[1]) {
+                if(gameBoard[preDropBlock[1][1] + 1][preDropBlock[1][0]] === "" || gameBoard[preDropBlock[1][1] + 1][preDropBlock[1][0]] === currentBlock[1]){
+                    if(gameBoard[preDropBlock[2][1] + 1][preDropBlock[2][0]] === "" || gameBoard[preDropBlock[2][1] + 1][preDropBlock[2][0]] === currentBlock[1]){
+                        if(gameBoard[preDropBlock[3][1] + 1][preDropBlock[3][0]] === "" || gameBoard[preDropBlock[3][1] + 1][preDropBlock[3][0]] === currentBlock[1]){
                             preDropBlock[0][1]++;
                             preDropBlock[1][1]++;
                             preDropBlock[2][1]++;
@@ -162,14 +183,13 @@ function preDrop() {
         } else {
             fix = true;
         }
-        
-    } while(fix == false);
+    } while(fix === false);
 
     for(let x = 0; x < preDropBlock.length; x++){
         for (let i = 0; i < gameBoard.length; i++) {
             for(let j = 0; j < gameBoard[0].length; j++){
-                if(i == preDropBlock[x][1] && j == preDropBlock[x][0]){
-                    if(gameBoard[i][j] == ""){
+                if(i === preDropBlock[x][1] && j === preDropBlock[x][0]){
+                    if(gameBoard[i][j] === ""){
                         gameBoard[i][j] = currentBlock[1].charAt(0) + "R"
                     }
                 }
